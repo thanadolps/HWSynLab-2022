e957635b276afcc34c6bff8d861f16bc0cc8a6de
ThanachatAim <88937843+ThanachatAim@users.noreply.github.com>
2022-12-14 14:18:13 +0700

    Add files via upload

---
+++ Final/Final.srcs/sources_1/new/alu.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 14.12.2022 14:03:29
+// Design Name: 
+// Module Name: alu
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module alu(
+
+    );
+endmodule
